5d11f0ae9d48dc62fc3be7cf828623095c1487cf
Ade-StapleHill <ade.perry@digilocal.org.uk>
2021-08-06 18:52:35 +0100

    choropleth now working and quicker as df.groupby().agg() is used for series stats.
    - Fixed map now displays site colour choropleth. Change was geojson 'id'
    is now integer index, previously was site tuple
    ('Study_number', 'Latitude', 'Longitude')
    
    - choropleth timeline remains dodgy. slider works if timestamp type
    changed from string to int, but then site colour does not change.
    
    - Added variable DBG for debug. Set DBG=True to dump some info and
    just display one site
    
    - Site has 'Country' and 'Region' cols so can find on map
    
    - Use df.groupby().agg() to get aggregate data for each study_number
    group, e.g.  'Rs_annual' min, max and 'date' nunique to filter out
    groups that have repeated dates.
    
    - Map has mutiple layers, 'Stamen Toner' is good for choropleth as plain
    
    - Map has popup markers
    
    - Use of stats.zscore() to remove outliers if std is greater than 3 sigma.
      - In this dataset filters out a site in Thailand that had a range
    way higher than all the other sites and was skewing the coloumap.
    
    - Plot a subset of  'Rs_annual' groups against a colourmap background
    which can be  used to check the choropleth colour. The choropleth colour
    change is good if just one site mapped (DBG=True), but is not correct
    when multiple sites mapped (site 'id: 0'  in North Wales does not
    change colour)

--- srdb_choro.py
+++ srdb_choro.py
@@ -4,11 +4,29 @@ References:
 https://nbviewer.jupyter.org/github/python-visualization/folium/blob/master/examples/TimeSliderChoropleth.ipynb
 https://geohackweek.github.io/ghw2018_web_portal_inlandwater_co2/InteractiveTimeSeries.html
 '''
-import pandas as pd
-import matplotlib.pyplot as plt
-from datetime import datetime 
 
 #%matplotlib inline
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+from datetime import datetime 
+
+#%%
+DBG = False # set True for debug
+vals_len_dbg = None  # set to count of sites to map, if not set then all sites
+group_count_min = 4 # set to minimun number of records per site (current max is 13)
+outfile='srdb_choro_out' # file prefix for outputs
+
+if DBG:
+    vals_len_dbg = 1
+
+#%% Test if run in notebook
+def is_interactive():
+    import __main__ as main
+    return not hasattr(main, '__file__')
+
 #%% 'Study_midyear' to timestamp
 def midyear_to_timestamp(key3):
     sec_in_year = (365*24*60*60)
@@ -28,7 +46,8 @@ def timestamp_date(timestamp):
 # %% initial df from csv file with 'date' col
 #--------------------------------------------
 group_cols = ['Study_number', 'Latitude', 'Longitude'] # site
-csv_cols = list((*group_cols, 'Study_midyear', 'Rs_annual'))
+csv_cols = group_cols + ['Country', 'Region', 'Study_midyear', 'Rs_annual']
+
 df_base = pd.read_csv('srdb-data-V5.csv', usecols=csv_cols)[csv_cols].dropna().astype(str)
 
 #%% convert 'Study_midyear' to datetime
@@ -39,10 +58,9 @@ df_base['Rs_annual'] = df_base['Rs_annual'].astype('float64')
 df_base['timestamp'] = df_base['timestamp'].astype('int64')
 df_base['date'] = pd.to_datetime(df_base['date'])
 
-#%% Sort by site and date
-sort_cols = list((*group_cols, 'date'))
-df_base.sort_values(sort_cols, inplace=True, ignore_index=True)
-df_base.head(10)
+#%% sort within groups on date
+df_base.sort_values(group_cols + ['date'], inplace=True, ignore_index=True)
+df_base.head(5)
 
 #%% group on site ('Study_number', 'Latitude', 'Longitude')
 ''' Some 'Study_number' have multiple very close sites with 
@@ -52,17 +70,58 @@ be part of the same site series. Ignore for now.
 grouped = df_base.groupby(group_cols) # e.g. index ('4391', '2.97', '102.3')
 
 #%% Display groups to eyeball look ok
-group_keys = list(grouped.groups)
-print(f"group_keys len={len(group_keys)}, first={group_keys[0]}, last={group_keys[-1]}") 
+if DBG:
+    group_keys = list(grouped.groups)
+    print(f"group_keys len={len(group_keys)}, first={group_keys[0]}, last={group_keys[-1]}") 
+    print(f"first get_group({group_keys[0]}) = {grouped.get_group(group_keys[0])}")
+    print(f"last get_group({group_keys[-1]}) = {grouped.get_group(group_keys[-1])}")
+
+#%% Rs_annual aggregate
+df_agg = grouped.agg({'Rs_annual': ['min', 'max', 'mean'], \
+            'date' : ['min', 'max', 'count', 'nunique']})
+df_agg.head()
+
+#%% For now, simply ignore groups that have repeated dates
+df_agg = df_agg[ df_agg[('date', 'count')] == df_agg[('date', 'nunique')] ]
+
+#%% Filter out those sites without sufficient history
+df_agg = df_agg[ df_agg[('date', 'count')] >= group_count_min ]
+
+#%% remove outliers, those with a mean that has a standard deviation greater than 3sigma 
+# https://stackoverflow.com/questions/23199796/detect-and-exclude-outliers-in-pandas-data-frame
+df_outliers = df_agg[ (np.abs(stats.zscore(df_agg[('Rs_annual', 'mean')])) > 3) ]
+df_outliers
+
+print(f"remove outliers {df_agg[ (np.abs(stats.zscore(df_agg[('Rs_annual', 'mean')])) >= 3) ].index}")
+
+df_agg = df_agg.drop(index=df_outliers.index)
+
+#%% Sort groups by count
+df_agg.sort_values(('date', 'count'), inplace=True, ascending=False)
+
+#%% Possibly reduce number of sites if DBG
+if vals_len_dbg != None: 
+    # restrict number sites when DBG for simplicity
+    if vals_len_dbg < len(df_agg):
+        df_agg = df_agg.iloc[0:vals_len_dbg]    # first n rows
+df_agg.head()
 
 #%%
-grouped.get_group(group_keys[0])
-#%%
-grouped.get_group(group_keys[-1])
+val_max = df_agg[('Rs_annual', 'max')].max()
+val_min = df_agg[('Rs_annual', 'min')].min()
+print(f"val_max={val_max}, val_min={val_min}")
 
-#%% Create df2_grouped from groups and sort datestamps within groups 
-''' For now, simply ignore groups that have repeated dates
+#%%
+records_init = len(df_base)
+records_remain = df_agg[('date', 'count')].sum()
+records_discarded = records_init - records_remain
+print(f"discarded {records_discarded} records, sites remain={len(df_agg)}" + \
+        f", records remain={records_remain}" + \
+        f" ({(float(records_remain) / float(records_init))*100:.1f} %)")
+
+#%% Obsolete code to remove groups with repeated date
 '''
+#----------------------------------------------------------
 df2_grouped = None
 for idx, (name, group) in enumerate(grouped):
     midyears = group['Study_midyear']
@@ -82,96 +141,78 @@ for idx, (name, group) in enumerate(grouped):
             df2_grouped = group_copied
         else:
             df2_grouped = df2_grouped.append(group_copied)
-#%% df2_grouped index is tuple ('Study_number', 'Latitude', 'Longitude')
-df2_grouped.reset_index(drop=True, inplace=True)
-df2_grouped.sort_values(group_cols, inplace=True, ignore_index=True)
-
-#df2_grouped.sort_values(group_cols, inplace=True, ignore_index=True)
-#df2_grouped.set_index(group_cols, inplace=True)
-
-#%%
-df2_grouped.head(10)
-
-#%%
-records_init = len(df_base)
-records_remain = len(df2_grouped)
-records_discarded = records_init - records_remain
-print(f"discarded {records_discarded} records due to repeated 'Study_midyear' in group, " + 
-            f"{records_remain} ({(float(records_remain) / float(records_init))*100:.1f} %) records remain")
-
-#%% 'Rs_annual' ranges 
-#---------------------
-grouped = df2_grouped.groupby(group_cols)
-df3_val = grouped.agg({'Rs_annual': ['min', 'max', 'count']})
-df3_val.columns = [col[1] for col in df3_val.columns]  # remove 'Rs_annual' outer level col
-df3_val.sort_values('count', inplace=True, ascending=False)
-df3_val.head()
-
-#%%
-''' only sites those with multiple timestamp count. This constricts number of 
-map markers to those with plenty of history
+#----------------------------------------
 '''
-group_count_min = 7
-if True:
-    df3_val = df3_val[ df3_val['count'] > group_count_min ]
-    print(f"Number of sites with timestamp series length greater than " +
-                f"{group_count_min} = {len(df3_val)}")
 
-#%% # restrict number sites when DBG for simplicity
-vals_len = len(df3_val)
-vals_len = 1    # restrict number of sites
+#%% Use df_agg index to filter original df
+df4_filtered = df_base.copy()
+df4_filtered.set_index(group_cols, inplace=True)
+df4_filtered = df4_filtered.loc[df_agg.index] # use df_agg index which is sorted by 'count'
+df4_filtered.reset_index(inplace=True)
 
-df3_val = df3_val.iloc[0:vals_len]
-df3_val.head()
+df4_filtered['site'] = df4_filtered['Study_number'] + '_' + \
+                    	df4_filtered['Latitude'] + '_' + \
+                        df4_filtered['Longitude']
+df4_filtered
+
+#%% histogram (value frequency) to decide colourmap step
+df4_filtered[['Rs_annual']].hist()
 
 #%% 
-# By now we df3_val for the index of groups to map, and df2_grouped for the 
-# values to use
-#------------------------
-
+'''
+# By now we have df_agg for the index of groups to map and stats of vals, 
+# and df4_filtered for the list of vals to use
+# ------------------------
+'''
 # %% Colour map
 from branca.colormap import linear
 
-val_max = df3_val['max'].max()
-val_min = df3_val['min'].min()
-cmap = linear.PuRd_09.scale(val_min, val_max)
+# Tried PuRd_09 range but low values indistinct. Instead use a rainbow range 
+cmap = linear.Spectral_11.scale(val_min, val_max) # PuRd_09
 
-print(f"val_max={val_max}, val_min={val_min}")
+#%% Avoid the few very large values skewing the linear colourmap
+# Use quantiles to equalise the number of values in each colour step
+cmap_step=11
+cmap = cmap.to_step(data=df4_filtered['Rs_annual'], n=cmap_step) # , method='quantiles')
+cmap
 
-#%% Create df of all groups matching df3_val index
-
-df4_filtered = pd.DataFrame()
-grouped = df2_grouped.groupby(group_cols)
-
-for i, (index, row) in enumerate(df3_val.iterrows()):
-    df_group = grouped.get_group(index).copy()
-    df_group['site'] = '_'.join(index)  # string from tuple, e.g. '3053_36.13_137.42'
-    df4_filtered = df4_filtered.append(df_group)
-
+#%%
 df4_filtered['colour'] = df4_filtered['Rs_annual'].apply(cmap)
-df4_filtered.reset_index(drop=True, inplace=True)
-
-#%%
-df4_filtered.head()
-
-#%%
-records_remain2 = len(df4_filtered)    
-records_discarded = records_remain - records_remain2
-print(f"discarded {records_discarded} records due to group_count={group_count_min} " + 
-        f"and vals_len={vals_len} filtering")
-print(f"remaining number of records = {records_remain2} ({(float(records_remain2) / float(records_init))*100:.1f}%)")
 
 #%%
 # Plot 'Rs_annual' of first group to check color map
 
 grouped = df4_filtered.groupby(group_cols)
-for idx, (group_name, df_group) in enumerate(grouped):
-    if idx > 0:
-        break # just do first one
-    title = '_'.join(group_name) # create string from tuple
-    colours = df_group['colour'].tolist()
-    df_group[['date', 'Rs_annual']].plot(x='date', y='Rs_annual', kind='bar', 
-                title=title, color=colours)
+df_group = grouped.get_group(df_agg.index[0])  
+colours = df_group['colour'].tolist()
+site = df_group['site'].tolist()[0]
+df_group[['date', 'Rs_annual']].plot(x='date', y='Rs_annual', kind='bar', 
+                title=site, color=colours)
+
+#%% Plot a subset of groups against a colourmap background for check
+fig, ax = plt.subplots()
+
+for idx, (name, group) in enumerate(grouped):
+    if idx > 5:
+        break   # plot first 5
+    site = group['site'].tolist()[0]
+    plt.plot(group['date'], group['Rs_annual'], 'x-', label=site)
+
+plt.legend(title='site', 
+            bbox_to_anchor=(1, 1),
+            bbox_transform=plt.gcf().transFigure)
+
+cmap_bucket = (val_max - val_min)/cmap_step
+for i in range(int(val_min), int(val_max), int(cmap_bucket)):
+    ax.axhspan(i, i+cmap_bucket, facecolor=cmap(i+0.5*cmap_bucket), alpha=0.7)
+plt.title('Rs_annual against colourmap for subset sites')
+
+if is_interactive():
+    plt.show()
+else:
+    outf = f"{outfile}_colours.png"
+    print(f"Saving file {outf}")
+    plt.savefig(outf)
 
 #%% df to geojson, df4_filtered contains all the sites to display
 ''' 
@@ -179,17 +220,23 @@ folium.TimeSliderChoropleth() does not work with geojson 'Point' geometry.
 Instead, create a (square) 'Polygon', see: 
 https://geoffboeing.com/2015/10/exporting-python-data-geojson/
 '''
-def coord_to_polygon(lon, lat):
-    # create polygon (square) from lon, lat string params
-    lon1 = str(float(lon)+0.2)
-    lat1 = str(float(lat)+0.1)
-    res = [
-        [lon,   lat], # bottom left
-        [lon1, lat], # bottom right
-        [lon1, lat1], # top right
-        [lon,   lat1],    # top left
-        [lon,   lat] # back to bottom left
+def coord_to_geometry(lon, lat):
+    # create polygon (square) from lon, lat centre
+    top_right = [str(float(lon)+0.1), str(float(lat)+0.05)]
+    bottom_left = [str(float(lon)-0.1), str(float(lat)-0.05)]
+
+    coords = [
+        bottom_left,
+        [top_right[0], bottom_left[1]], # bottom right
+        top_right,
+        [bottom_left[0], top_right[1]],    # top left
+        bottom_left # back to bottom left
     ]
+    res={}
+    if False:
+        res = {'type' : 'Point', 'coordinates': coords[0]}   # DBG
+    else:
+        res = {'type' : 'Polygon', 'coordinates': [coords]}
     return res
 #%% 
 def df_to_geojson(df, properties=[], lat_col='Latitude', 
@@ -199,79 +246,130 @@ def df_to_geojson(df, properties=[], lat_col='Latitude',
         feature = {'id':index,
                    'type':'Feature',
                    'properties':{},
-                   'geometry':{'type':'Polygon',
-                               'coordinates':[]}}
-        feature['geometry']['coordinates'] = [
-            coord_to_polygon(row[lon_col],row[lat_col])
-        ]
+                   'geometry': coord_to_geometry(row[lon_col],row[lat_col])}
         for prop in properties:
             feature['properties'][prop] = row[prop]
         geojson['features'].append(feature)
     return geojson
 
-# %% df from groupby
-'''
-Create a df indexed by 'site' for use by geojson
-'''
-site_cols = list((*group_cols, 'site'))
-df5_geojson = df4_filtered[site_cols].copy()
+# %% Create a geojson df indexed by grouby order
+properties_cols = ['site', 'Country', 'Region']  # extra info in geojson
+df5_geojson = df4_filtered[group_cols + properties_cols].copy()
 
-df5_geojson['count'] = 1 # temp val
-df5_geojson = df5_geojson.groupby(site_cols).count() # count will be len of each col
+df5_geojson = df5_geojson.groupby(group_cols).first()
+df5_geojson = df5_geojson.loc[df_agg.index] # reorder to match df_agg
+
+val_cols = [('Rs_annual', 'min'), ('Rs_annual', 'max'), 
+            ('Rs_annual', 'mean'),  ('date', 'count'),
+            ('date', 'min'), ('date', 'max'), ]
+prop_val_cols = ['min',  'max', 
+            'mean', 'count',
+            'start_date', 'end_date']
+
+df5_geojson[prop_val_cols] = df_agg[val_cols].astype(str)
+df5_geojson['mean'] = df5_geojson['mean'].astype('float64').apply(lambda x: f"{x:.1f}") # one decimal place
+properties_cols = properties_cols + prop_val_cols
 
 df5_geojson.reset_index(inplace=True)
-df5_geojson.set_index('site', inplace=True)
-
 df5_geojson.head()
 
-#%% Convert df to json
-geo_json = df_to_geojson(df5_geojson, ['Study_number'])
-features_len = len(geo_json['features'])
-print(f"geo_json features len={features_len}, last feature={geo_json['features'][-1]}")
+#%% Convert to json
+geo_json = df_to_geojson(df5_geojson, properties_cols)
 
-#%%
+#%% sanity check geo_json
+if DBG:
+    features_len = len(geo_json['features'])
+    print(f"geo_json features len={features_len}, features[0]={geo_json['features'][0]}")
+    for idx, feature in enumerate (geo_json['features']):
+        p = geo_json['features'][idx]['properties']
+        print(f"features id={feature['id']}, site={p['site']}, location={p['Region']}, {p['Country']}")
+
+#%% stle_dict is site colour history
+#---------------------------------------------
 '''
 Format of styledict:
-'site' col used as index in df5_geojson which is then assigned 
-to geojson 'features'.'id'
-{<site> : {
+<idx> is group index in df5_geojson which is assigned 
+to geojson 'features'.'id' key
+{<idx> : {
     <timestamp> : {
         color : <cmap derived from Rs_annual>,
         opacity : 0.5
     }
 },
-
+Note: choropleth timeslider is out of order if timestamp is string type. It is
+the correct order if numeric type, however the map does not show the site
 '''
 style_dict = {}
-grouped = df4_filtered.groupby('site')
+grouped = df4_filtered.groupby(group_cols)
 
-for group_name, df_group in grouped:
+for idx, (group_name, df_group) in enumerate(grouped):
     timestamp_dict = {}
-    for index, row in df_group.iterrows():
+    for row_index, row in df_group.iterrows():
         color_dict = {}
         color_dict['color'] = row['colour']
         color_dict['opacity'] = 0.7
         timestamp_dict[f"{row['timestamp']:.0f}"] = color_dict
-    style_dict[group_name] = timestamp_dict
+        #timestamp_dict[row['timestamp']] = color_dict # DBG
+    style_dict[idx] = timestamp_dict
 
-for idx, key in enumerate(style_dict):
-    if idx > 0:
-        break   # just display first one
-    for idx2, key2 in enumerate(style_dict[key]): 
-        if idx2 > 0:
+if DBG:
+    for idx, key in enumerate(style_dict):
+        if idx > 0:
             break   # just display first one
-        print(f"style_dict[{key}][{key2}] = {style_dict[key][key2]}")
-# %%
-import folium
-from folium.plugins import TimeSliderChoropleth
+        for idx2, key2 in enumerate(style_dict[key]): 
+            if idx2 > 0:
+                break   # just display first one
+            print(f"style_dict[{key}][{key2}] = {style_dict[key][key2]}")
 
-m = folium.Map([0, 0], tiles="Stamen Toner", zoom_start=2)
+# %% ref: https://stackoverflow.com/questions/53898733/how-to-customize-the-layercontrol-in-folium
+import folium
+from folium.plugins import TimeSliderChoropleth, MarkerCluster
+
+#%% map marker popup text using html <br> for embedded newline
+def map_markers(df, m, prop_cols=[]):
+    for index, row in df.iterrows():
+        props = ',<br> '.join((f"{i} = {row[i]}" for i in prop_cols))
+        folium.Marker(
+            location=[row['Latitude'], row['Longitude']],
+            popup=f"id = {index},<br>{props}").add_to(m)
+
+
+#%% Generate map with multiple layers
+first_row = df5_geojson.iloc[0]
+map = folium.Map([first_row['Latitude'], first_row['Longitude']], 
+                                zoom_start=2)
+folium.TileLayer('openstreetmap').add_to(map)
+folium.TileLayer('Stamen Terrain').add_to(map) # terrain
+folium.TileLayer('Stamen Toner').add_to(map)  # plain map good for showing colourmap
+tile = folium.TileLayer(
+        tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        attr = 'Esri',
+        name = 'Esri Satellite',
+        overlay = False,
+        control = True
+       ).add_to(map)    # Satetlite
+
+fg=folium.FeatureGroup(name='Rs_annual choropleth', show=True)
+map.add_child(fg)
+choro_cluster = MarkerCluster().add_to(fg)
 g = TimeSliderChoropleth(
     geo_json,
     styledict=style_dict,
-).add_to(m)
+).add_to(choro_cluster)
 
-m.save('srdb_choro.html')
-m
+fg2=folium.FeatureGroup(name='Rs_annual site', show=True)
+map.add_child(fg2)
+marker_cluster = MarkerCluster().add_to(fg2)
+map_markers(df5_geojson, marker_cluster, properties_cols)
+
+cmap.add_to(map) # Add colour map 
+
+folium.LayerControl().add_to(map)
+
+outf=f"{outfile}.html"
+map.save(outf)
+map
+
+print(f"Created file {outf} with {len(grouped)} sites")
 
 # %%
